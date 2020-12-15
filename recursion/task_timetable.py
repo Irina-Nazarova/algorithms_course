e284d4ee8aea6b2cf51ac6b725838b35c0e6ba68
Irina <irina.g.nazarova@mail.ru>
2020-12-15 20:39:28 +0300

    add trees and updated comments to tasks

--- recursion/task_timetable.py
+++ recursion/task_timetable.py
@@ -2,7 +2,7 @@ def optimal_time_table(timetable: list) -> list:
     """
     :param timetable: [(9.0, 10.0), (9.3, 10.3), (10.0, 11.0), (10.3, 11.3)]
     :return: [(9.0, 10.0), (10.0, 11.0)]
-    вывод расписания, в котором можно будет провести как можно больше уроков.
+    schedule with the largest number of lessons.
     """
 
     timetable.sort(key=lambda x: (x[1], x[0]))
