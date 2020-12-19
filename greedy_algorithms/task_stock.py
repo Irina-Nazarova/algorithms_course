5be651392c9717b94e0d6f85d5f99008b6da0183
Irina <irina.g.nazarova@mail.ru>
2020-12-19 17:36:21 +0300

    update task_stock

--- greedy_algorithms/task_stock.py
+++ greedy_algorithms/task_stock.py
@@ -1,28 +1,21 @@
 def stock_profit(stock_prices):
     """
     :param stock_prices: [7 1 5 3 6 4]
-    :return: 7 - Нужно вывести число, равное максимально возможной прибыли за эти дни.
-        Если акций нет на руках: Покупаю если сегодня цена меньше чем завтpа. continue
-        Если акция есть на руках: Продаю если цена завтра меньше чем сегодня. continue
-        В остальных случаях ничего не предпринимаю.
+    :return: 7 - You need to display a number equal to the maximum possible profit for these days.
     """
 
     stock = "empty"
     stock_salary = 0
     for i in range(len(stock_prices) - 1):
-        # Купленных акций нет
         if stock == "empty":
             if stock_prices[i] < stock_prices[i + 1]:
-                # Если цена сегодня меньше чем завтра - покупаем
                 stock = stock_prices[i]
 
-        # Купленные акции есть
         else:
-            # Решаем, что сегодня делать с акцией
-            # Если завтра она стоит меньше - продаем
             if stock_prices[i] > stock_prices[i + 1]:
                 stock_salary += stock_prices[i] - stock
                 stock = "empty"
+
     return stock_salary
 
 
